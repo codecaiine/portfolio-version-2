908cc8bfc969d118a1d571f79cf6d1930bd36877
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-03-27 21:14:54 +0000

    Create first object

---
+++ backend/schemas/testimonials.js
@@ -0,0 +1,10 @@
+export default {
+    name: 'testimonials',
+    title: 'Testimonials',
+    type: 'document',
+    fields: [{
+        name: 'name',
+        title: 'Name',
+        type: 'string'
+    }, ],
+}
