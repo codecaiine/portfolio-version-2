0f47acd46bbbdcb10e7ffc4323b77627a7586d1f
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-03-27 23:05:22 +0000

    Update schema

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -18,7 +18,6 @@ export default createSchema({
   name: 'default',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([
-    testimonials
+  types: schemaTypes.concat([works, testimonials, brands, abouts, skills, workExperience, experiences, contact
   ]),
 })
