5deaf50b3651e189fe631d93875693a7663c79c6
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-03-27 21:37:42 +0000

    Add testimonials into schema

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -12,6 +12,6 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    testimonials
   ]),
 })
