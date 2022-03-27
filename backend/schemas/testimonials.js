9412dbd9ead633d142a6078d446d86cfee2aeefc
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-03-27 21:31:24 +0000

    Create testimonials objects

--- backend/schemas/testimonials.js
+++ backend/schemas/testimonials.js
@@ -2,9 +2,29 @@ export default {
     name: 'testimonials',
     title: 'Testimonials',
     type: 'document',
-    fields: [{
+    fields: [
+    {
         name: 'name',
         title: 'Name',
         type: 'string'
-    }, ],
+    },
+    {
+        name: 'company',
+        title: 'Company',
+        type: 'string'
+    },
+    {
+        name: 'imageurl',
+        title: 'ImgURL',
+        type: 'image',
+        options: {
+            hotspot: true,
+        }
+    },
+    {
+        name: 'feedback',
+        title: 'Feedback',
+        type: 'string'
+    },
+ ],
 }
