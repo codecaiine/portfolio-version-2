8103e8bc013f845aecdbcb958bc3edcace584025
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-03-27 23:02:51 +0000

    Import contact

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -10,6 +10,7 @@ import abouts from './abouts'
 import experiences from './experiences'
 import skills from './skills'
 import workExperience from './workExperience'
+import contact from './contact'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
