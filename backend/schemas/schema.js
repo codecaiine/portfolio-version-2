702ff398f8a14959b30abf87d4db434c041f405e
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-03-27 22:58:29 +0000

    Import brands

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -5,6 +5,7 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 import works from './works'
 import testimonials from './testimonials'
+import brands from './brands'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
