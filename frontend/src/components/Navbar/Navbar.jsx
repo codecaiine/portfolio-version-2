03440eee90faa088b2c6b3d2ac9d3f2cb8c0cfbc
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-03-30 22:07:03 +0000

    Add nav and image tag

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,11 @@ import './Navbar.scss';
 
 const Navbar = () => {
     return (
-        <div> Navbar </div>
+        <nav>
+            <div>
+                <img src={images.logo} alt="logo" />
+            </div>
+        </nav>
     )
 }
 
