97b788c1d85ce4d51cf45a6047711b4c170c4229
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-03-31 22:39:22 +0000

    Add css class name with bem methodology

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,19 +5,18 @@ import './Navbar.scss';
 
 const Navbar = () => {
     return (
-        <nav>
-            <div>
-                <img src={images.logo} alt="logo" />
-            </div>
-            <ul>
-            {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
-                <li key={item}>
-                  <a href={`#${item}`}>
-                    {item}
-                  </a>
-                </li>
-              ))}
-            </ul>
+        <nav className="app__nav">
+        <div className="app__nav-logo">
+          <img src={images.logo} alt="logo" />
+        </div>
+        <ul className="app__nav-link">
+          {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+            <li className="app__flex p-text" key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          ))}
+        </ul>
         </nav>
     )
 }
