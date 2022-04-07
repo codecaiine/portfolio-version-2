bb673c9b332c3981de603bbd07c2c0fb93b7b863
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-04-07 22:59:00 +0000

    Update navigation jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -6,30 +6,30 @@ import { images } from '../../constants';
 import './Navbar.scss';
 
 const Navbar = () => {
-const [toggle, setToggle] = useState(false);
-  
-    return (
-        <nav className="app__nav">
-        <div className="app__nav-logo">
-          <img src={images.logo} alt="logo" />
-        </div>
-        <ul className="app__nav-links">
-          {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
-            <li className="app__flex p-text" key={`link-${item}`}>
-              <div />
-              <a href={`#${item}`}>{item}</a>
-            </li>
-          ))}
-        </ul>
-        
-        <div className="app__nav-menu">
-         <HiMenuAlt4 onClick={() => setToggle(true)} />
+  const [toggle, setToggle] = useState(false);
 
-         {toggle && (
-           <motion.div
-              whileInView={{ x: [300, 0] }}
-              transition={{ duration: 0.85, ease: 'easeOut' }}
-            >
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
             <HiX onClick={() => setToggle(false)} />
             <ul>
               {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
@@ -40,13 +40,11 @@ const [toggle, setToggle] = useState(false);
                 </li>
               ))}
             </ul>
-           </motion.div>
-           )}
+          </motion.div>
+        )}
+      </div>
+    </nav>
+  );
+};
 
-        </div>
-
-        </nav>
-    )
-}
-
-export default Navbar
+export default Navbar;
