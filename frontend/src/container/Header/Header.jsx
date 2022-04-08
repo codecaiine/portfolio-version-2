d48a2cd90917c0aab5bc9e6ffc7f8e9c0e0ed2ae
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-04-08 22:16:24 +0000

    Fix class errors

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -6,27 +6,30 @@ import './Header.scss';
 
 const Header = () => {
     return (
+        <div className="app__header app__flex">
         <motion.div
-        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-        transition={{ duration: 0.5 }}
-        className="app__header-info"
+          whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+          transition={{ duration: 0.5 }}
+          className="app__header-info"
         >
-        <div className="app__header-badge">
-        <div className="badge-cmp app__flex">
-          <span>👋</span>
-          <div style={{ marginLeft: 20 }}>
-            <p className="p-text">Hey, I am</p>
-            <h1 className="head-text">Yannick-Noel</h1>
+          <div className="app__header-badge">
+            <div className="badge-cmp app__flex">
+              <span>👋</span>
+              <div style={{ marginLeft: 20 }}>
+                <p className="p-text">Hey, I am</p>
+                <h1 className="head-text">Yannick-Noel</h1>
+              </div>
+            </div>
+    
+            <div className="tag-cmp app__flex">
+              <p className="p-text">Software Engineer</p>
+              <p className="p-text">Tech Entreprene</p>
+            </div>
           </div>
-        </div>
-
-        <div className="tag-cmp app__flex">
-          <p className="p-text">Software Engineer</p>
-          <p className="p-text">Tech Entrepreneur</p>
-        </div>
-        </div>
-
         </motion.div>
+
+        </div>
+        
     )
 }
 
