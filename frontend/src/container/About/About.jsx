33264895516d60b2962b3cccf49cbba8c19f0f97
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-04-10 21:19:11 +0000

    Create new constant abouts

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -4,6 +4,9 @@ import { motion } from 'framer-motion';
 import './About.scss';
 
 const About = () => {
+
+    const abouts = [];
+
     return (
         <> 
          <h2 className="head-text">I Know that <span>Clean Code</span> <br />means  <span>Good Code</span></h2>
