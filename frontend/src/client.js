28395447ce91ae7d2bdf478e5fa22e895b428c58
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-04-10 22:51:30 +0000

    Add empty parameters

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -2,5 +2,9 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-    
+    projectId: '',
+    dataset: '',
+    apiVersion: '',
+    useCdn: true,
+    token: '',
 });
