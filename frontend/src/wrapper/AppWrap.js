f4713bc502a0a444ef296ecb85189c2ea565e249
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-04-11 23:26:06 +0000

    Add new div with class names

--- frontend/src/wrapper/AppWrap.js
+++ frontend/src/wrapper/AppWrap.js
@@ -5,6 +5,10 @@ const AppWrap = (Component, idName, classNames) => function HOC() {
     return (
         <div id={idName} className={`app__container ${classNames}`}>
            <SocialMedia />
+
+           <div className="app__wrapper app__flex">
+               
+           </div>
         </div>
     )
 }
