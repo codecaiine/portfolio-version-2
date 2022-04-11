b8270c2324a542416043683d2c75a33fac8de678
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-04-11 23:30:11 +0000

    Add copyright information

--- frontend/src/wrapper/AppWrap.js
+++ frontend/src/wrapper/AppWrap.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from 'react';
 import { NavigationDots, SocialMedia } from '../components';
 
 const AppWrap = (Component, idName, classNames) => function HOC() {
-    return (
-        <div id={idName} className={`app__container ${classNames}`}>
-           <SocialMedia />
+  return (
+    <div id={idName} className={`app__container ${classNames}`}>
+      <SocialMedia />
+      <div className="app__wrapper app__flex">
+        <Component />
 
-           <div className="app__wrapper app__flex">
-               
-           </div>
+        <div className="copyright">
+          <p className="p-text">@2022 Codecaine</p>
+          <p className="p-text">All rights reserved</p>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default AppWrap
+export default AppWrap;
