e8b7513025021a28d8da65bf47b82e058bf04269
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-04-11 22:14:57 +0000

    Add project id variable

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -2,7 +2,7 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-    projectId: 'im8lopbc',
+    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
     apiVersion: '2022-02-01',
     useCdn: true,
