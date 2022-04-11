c6ada04cb87fe61bd4602f02bf9fe7b6fcdfa2db
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-04-11 23:23:50 +0000

    Add class name

--- frontend/src/wrapper/AppWrap.js
+++ frontend/src/wrapper/AppWrap.js
@@ -3,7 +3,7 @@ import { NavigationDots, SocialMedia } from '../components';
 
 const AppWrap = (Component, idName, classNames) => function HOC() {
     return (
-        <div id={idName} >
+        <div id={idName} className={`app__container ${classNames}`}>
 
         </div>
     )
