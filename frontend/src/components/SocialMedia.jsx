6d5a2142966b7a1aaeba44afb09f395ffb9693f4
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-04-11 22:58:02 +0000

    Wrap instagram icon

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import { BsTwitter, BsInstagram } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
-const SocialMedia = () => {
-    return (
-        <div className="app__social">
-            <div>
-               <BsTwitter />
-            </div>
-            <div>
-               <FaFacebookF />
-            </div>
-        </div>
-    )
-}
-
-export default SocialMedia 
+const SocialMedia = () => (
+    <div className="app__social">
+      <div>
+        <BsTwitter />
+      </div>
+      <div>
+        <FaFacebookF />
+      </div>
+      <div>
+        <BsInstagram />
+      </div>
+    </div>
+  );
+  
+  export default SocialMedia;
