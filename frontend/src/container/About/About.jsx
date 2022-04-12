f5d56a92c5676f2359a25563cc22a37d041bbc75
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-04-12 20:30:11 +0000

    Import wrap

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
+import { AppWrap } from '../../wrapper';
 import './About.scss';
 import { urlFor, client } from '../../client';
 
